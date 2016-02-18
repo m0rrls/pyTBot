f70ae65dd90c92b96f944aefde3b753594017022
piocze <piotr.telman@gmail.com>
2016-02-18 21:05:19 +0100

    dodano polaczenie z sqlite

--- users.py
+++ users.py
@@ -1,12 +1,13 @@
 import requests
 import string
+import sqlite3
 from bs4 import BeautifulSoup
 
-page = requests.get("https://tmi.twitch.tv/group/user/forsenlol/chatters")
+page = requests.get("https://tmi.twitch.tv/group/user/erroreq/chatters")
 page = page.content
 lines = string.split(page, "\n")
 
-control = 0
+"""control = 0
 for item in lines:
 	if control == 0 and item.find("chatters")!=-1:
 		control = 1
@@ -17,7 +18,12 @@ for item in lines:
 			tmp = ord(i)	
 			if (tmp>96 and tmp<123) or (tmp>48 and tmp<58):
 				nick+=i
-		"""if nick == "erroreq":
+		if nick == "erroreq":
 			print nick
-			exit()"""
-		print nick
+			exit()
+		print nick"""
+
+db = sqlite3.connect("sqlite.db")
+cursor = db.cursor()
+cursor.execute('SELECT POINTS FROM POINTS WHERE NICK = \'test\'')
+print cursor.fetchone()
